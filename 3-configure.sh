20a75d48961ad1e97a93712a4a7bde4db906a985
Juan González <j-gonzalezo@email.de>
2019-11-23 23:41:02 +0700

    new

--- 3-configure.sh
+++ 3-configure.sh
@@ -6,13 +6,13 @@
 
 echo "copy files"
 
-#mv .git .git_
+mv .git .git_
 
-#find /home/yoo/sinox -type f -print0 | xargs -0 sudo chmod -R 644
-#find /home/yoo/sinox -type d -print0 | xargs -0 sudo chmod -R 755
+find /home/yoo/sinox -type f -print0 | xargs -0 sudo chmod -R 644
+find /home/yoo/sinox -type d -print0 | xargs -0 sudo chmod -R 755
 
-#chown -R yoo:yoo /home/yoo/sinox/home/ *
-#chown -R yoo:yoo /home/yoo/sinox/home/ .
+chown -R yoo:yoo /home/yoo/sinox/home/ *
+chown -R yoo:yoo /home/yoo/sinox/home/ .
 
 chmod +x /home/yoo/sinox/home/docklight_3.0.orig/src/docklight 
 
@@ -21,8 +21,7 @@ cp /home/yoo/sinox/home/automount.pkla /etc/polkit-1/localauthority/50-local.d/a
 
 cp -rT /home/yoo/sinox/home /home/yoo
 
-
-
+rm -rf /home/yoo/sinox
 echo "done"
 exit
 
