35a12dc578836a25e935106fde9fc42a614f2eb9
Juan González <j-gonzalezo@email.de>
2019-11-24 01:10:24 +0700

    permisions for copy

--- 3-configure.sh
+++ 3-configure.sh
@@ -6,7 +6,7 @@
 
 echo "copy files"
 
-#v .git .git_
+mv .git .git_
 
 find /home/yoo/sinox -type f -print0 | xargs -0 sudo chmod -R 644
 find /home/yoo/sinox -type d -print0 | xargs -0 sudo chmod -R 755
@@ -16,12 +16,14 @@ chown -R yoo:yoo /home/yoo/sinox/home/ .
 
 chmod +x /home/yoo/sinox/home/docklight_3.0.orig/src/docklight 
 
-chown root:root /home/yoo/sinox/automount.pkla
-cp /home/yoo/sinox/home/automount.pkla /etc/polkit-1/localauthority/50-local.d/automount.pkla
+chown root:root /home/yoo/automount.pkla
+cp -p /home/yoo/sinox/automount.pkla /etc/polkit-1/localauthority/50-local.d/automount.pkla
 
-cp -rT /home/yoo/sinox/home /home/yoo
+cp -rT -p /home/yoo/sinox/home /home/yoo
 
 #m -rf /home/yoo/sinox
+
+
 echo "done"
 exit
 
