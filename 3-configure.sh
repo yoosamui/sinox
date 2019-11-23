8f8cf43507705a1ef5e554625529381d97f2f081
Juan González <j-gonzalezo@email.de>
2019-11-23 22:12:56 +0700

    modification

--- 3-configure.sh
+++ 3-configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #--------------------
-# needs sudo access
+# needs root access
 #--------------------
 
 echo "copy files"
@@ -11,11 +11,10 @@ find /home/yoo/sinox/home/ -type f -print0 | xargs -0 sudo chmod 644
 find /home/yoo/sinox/home/ -type d -print0 | xargs -0 sudo chmod 755
 find /home/yoo/sinox/home/ -type f -print0 | xargs -0 sudo chmod -x
 
-chown yoo:yoo home/*
+chown -R yoo:yoo /home/yoo/sinox/home/ *
+chown -R yoo:yoo /home/yoo/sinox/home/ .
 
 cp -rT /home/yoo/sinox/home /home/yoo
-
-
 
 
 echo "done"
