b6124242d86b445a7d0d3f470f7af1101f58354b
Juan González <j-gonzalezo@email.de>
2020-01-23 15:32:30 +0700

    update

--- 1-add-sudo.sh
+++ 1-add-sudo.sh
@@ -7,14 +7,13 @@
 echo "get the sudo package"
 apt install sudo
 
-echo "# yoo user privilege specification" >> /etc/sudoers
-echo "yoo	ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-echo "done"
-exit
-
-
-
-
-
+# sed chars
+p1='s/'
+p2='/g'
+r='/'
+l='USER'
+c=${p1}${l}${r}$USER${p2}
+file='etc/sudoers'
+echo ${c}
+sed -i ${c} ${file}
 
