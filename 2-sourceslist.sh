6a44f5869eb74e37940089f8f51d2cde27885e8d
Juan González <j-gonzalezo@email.de>
2020-01-23 19:28:12 +0700

    source.list

---
+++ 2-sourceslist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#--------------------
+# needs root access
+#--------------------
+
+cp -p "etc/sources.list" "/etc/apt/sources.list"
+echo "done"	
+	
---
+++ 3-plymouth.sh
@@ -0,0 +1,25 @@
+#1/bin/bash
+
+# run as root
+modules='/etc/initramfs-tools/modules'
+cp -p 'etc/modules' ${modules}
+grub='/etc/default/grub'
+
+cp -p 'etc/grub' ${grub}
+
+apt install plymouth -y
+apt-get install plymouth plymouth-themes -y
+
+/usr/sbin/plymouth-set-default-theme --list
+# Then, to set your desired theme run: 
+/usr/sbin/plymouth-set-default-theme -R joy
+
+# If this command errors out with a message saying that firmware files are missing, you will have to install the package
+# firmware-linux-nonfree. To do that, execute the following command:
+update-grub2
+update-initramfs -u
+apt-get install firmware-linux-nonfree -y
+update-initramfs -u
+
+
+
