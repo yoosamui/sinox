557d8f4695a319d8b03eb5aa60cc9ad076087b80
Juan González <j-gonzalezo@email.de>
2020-01-23 18:00:14 +0700

    soft

--- 4-configure.sh
+++ 4-configure.sh
@@ -18,6 +18,7 @@ chown -R $USER:$USER home/ .
 chmod a+x home/docklight_3.0.orig/src/docklight 
 
 chown root:root automount.pkla
+mkdir -p /etc/polkit-1/localauthority/50-local.d/automount.pkla
 cp -p automount.pkla /etc/polkit-1/localauthority/50-local.d/automount.pkla
 
 cp -rT -p home /home/yoo
