fcb7cce76905cc636734753c542ad51769fe11da
Juan González <j-gonzalezo@email.de>
2020-01-24 11:48:00 +0700

    chnages

--- 2-sources_list.sh
+++ 2-sources_list.sh
@@ -3,18 +3,5 @@
 #--------------------
 # needs root access
 #--------------------
+cp -p etc/sources.list /etc/sources.list
 
-
-
-# sed chars
-p1='s/'
-p2='/g'
-r='/'
-l='main'
-file='/etc/apt/sources.list'
-c=${p1}${l}&{r}$"main contrib non-free"${p2}
-sed -i ${c} $file
-echo "done"	
-
-
-	
