2e1b0b0246cfe99a07b4b2175164f98bd4901766
Juan González <j-gonzalezo@email.de>
2020-01-26 02:00:06 +0700

    skype

--- 6-software-extra.sh
+++ 6-software-extra.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+read -p "this script muss be run as sudo"
+
+
 # virtualbox
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian buster contrib"
+add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian buster contrib"
 
 apt update
-sudo apt install virtualbox-6.0 -y
+apt install virtualbox-6.0 -y
 add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian buster contrib"
 
 # skype
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 apt update
-sudo dpkg -i skypeforlinapux-64.deb
+
+dpkg -i skypeforlinapux-64.deb
 #rm -f skypeforlinux-64.deb
 
 echo "done"
