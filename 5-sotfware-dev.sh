7cd0a08edbf670d3d20da0d7209d102e0d96783a
Juan González <j-gonzalezo@email.de>
2020-01-26 00:04:40 +0700

    drivers

--- 5-sotfware-dev.sh
+++ 5-sotfware-dev.sh
@@ -73,8 +73,26 @@ vlc \
 chrony \
 notification-daemon -y
 
-apt install gnupg gnupg2 gnupg1
+apt install gnupg gnupg2 gnupg1 -y
+
 apt update
 apt upgrade -y 
 
-read -p "Please install drivers!!! reboot now !!!"
+# virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian buster contrib"
+
+apt update
+sudo apt install virtualbox-6.0 -y
+add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian buster contrib"
+
+# skype
+wget https://repo.skype.com/latest/skypeforlinux-64.deb
+dpkg -i skypeforlinapux-64.deb
+rm -f skypeforlinux-64.deb
+
+
+
+echo "done"
+
