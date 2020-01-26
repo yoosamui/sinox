c5e7fc710dc71d0fbf5f9b72bff348d511de6bc3
Juan González <j-gonzalezo@email.de>
2020-01-26 16:57:21 +0700

    menu

--- 6-software-extra.sh
+++ 6-software-extra.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # skype
-wget https://repo.skype.com/latest/skypeforlinux-64.deb
-sudo dpkg -i skypeforlinapux-64.deb
+#wget https://repo.skype.com/latest/skypeforlinux-64.deb
+#sudo dpkg -i skypeforlinapux-64.deb
 
 # virtualbox
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
