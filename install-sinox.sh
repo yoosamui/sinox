7610e7e0be48b73dbd1d87ca02dd6a7d120bb8ee
Juan González <j-gonzalezo@email.de>
2020-01-26 17:25:04 +0700

    menu

---
+++ install-sinox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sh ./1-add-sudo.sh
+sh ./2-sources_list.sh
+sh ./3-plymouth.sh
+sh ./4-copy-files.sh
+sh ./5-sotfware-dev.sh
+sh ./6-software-extra.sh
+sh ./7-optimizer.sh
+
+echo "done"
+
