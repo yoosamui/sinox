7414470c8128799f728e321de2abe976aff86f0b
Juan González <j-gonzalezo@email.de>
2020-01-27 22:14:13 +0700

    multi user

--- 4-copy-files.sh
+++ 4-copy-files.sh
@@ -8,8 +8,8 @@ echo "copy user files"
 
 # copy home
 cp -r home/. /home/$USER/
-exit
 
+echo "change HOME permisions"
 #chown $USER:$USER -R /home/$USER/.
 find /home/$USER -type f -print0 | xargs -0 chown -R $USER:$USER *
 find /home/$USER -type d -print0 | xargs -0 chown -R $USER:$USER *
@@ -17,11 +17,13 @@ find /home/$USER -type d -print0 | xargs -0 chown -R $USER:$USER *
 find /home/$USER -type f -print0 | xargs -0 chmod -R 644
 find /home/$USER -type d -print0 | xargs -0 chmod -R 755
 
+echo "copy var"
 # copy to var
 mkdir -p /var/sinox
-cp var/sinox/. /var/sinox
-chown $USER:$USER /var/sinox
+cp var/sinox/. /var/sinox/
+echo "change var permisions"
 
+chown $USER:$USER /var/sinox
 find /var/sinox/docklight-3.0 -type f -print0 | xargs -0 chown -R $USER:$USER *
 find /var/sinox/docklight-3.0 -type d -print0 | xargs -0 chown -R $USER:$USER *
 chmod a+x /var/sinox/docklight-3.0/docklight
@@ -38,7 +40,7 @@ find /var/sinox/scripts -type f -print0 | xargs -0 chmod a+x
 
 #chmod a+x home/docklight_3.0.orig/src/docklight 
 
-
+echo "copy automount.pkla"
 chown root:root automount.pkla
 mkdir -p /etc/polkit-1/localauthority/50-local.d/automount.pkla
 cp -p automount.pkla /etc/polkit-1/localauthority/50-local.d/automount.pkla
@@ -47,6 +49,7 @@ cp -p automount.pkla /etc/polkit-1/localauthority/50-local.d/automount.pkla
 #mkdir -p /usr/share/dbus-1/services
 #cp -p services/org.freedesktop.Notifications.service /usr/share/dbus-1/services
 
+echo "copy themes and change permisions"
 mkdir -p /usr/share/themes
 chown -R root:root /usr/share/themes
 
