48bebf7e9779b9b4ca381a3cb45d5858c66a4ceb
yoo <yoo@sinox.sinox.com>
2020-01-31 06:24:30 -0500

    optimizer

--- 7-optimizer.sh
+++ 7-optimizer.sh
@@ -3,15 +3,18 @@
 apt update
 
 # Add debian-breezetheme to alternatives with higher priority than debian packages:
-sudo update-alternatives --install /usr/share/sddm/themes/debian-theme sddm-debian-theme /usr/share/sddm/themes/debian-breeze 90
-sudo update-alternatives --config sddm-debian-theme
+update-alternatives --install /usr/share/sddm/themes/debian-theme sddm-debian-theme /usr/share/sddm/themes/debian-breeze 91
+#update-alternatives --config sddm-debian-theme
 
 # services
 systemctl enable sddm.service 
-sudo systemctl disable NetworkManager-wait-online.service
-sudo systemctl disable smbd
-sudo systemctl disable systemd-timesyncd.service
-sudo systemctl enable chrony
+systemctl disable NetworkManager-wait-online.service
+systemctl disable smbd
+systemctl disable systemd-timesyncd.service
+systemctl disable nmbd.service
+systemctl disable nvidia-persistenced.service
+
+systemctl enable chrony
 
 echo "done"
 
