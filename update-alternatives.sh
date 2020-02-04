e623007f6018cd6bc7cd5b1c7740160e99bd44e3
yoo <yoo@sinox.sinox.com>
2020-02-05 05:53:27 +0700

    new

---
+++ update-alternatives.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#--------------------
+# needs root access
+#--------------------
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/gedit  100
+update-alternatives --set editor /usr/bin/gedit
+sudo update-alternatives --config editor
+
+
+
