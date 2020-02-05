b72b8fecb82e8378276b32955238e71d3b2dc9e5
yoo <yoo@sinox.sinox.com>
2020-02-05 19:00:19 +0700

    docker

--- docker.sh
+++ docker.sh
@@ -5,8 +5,8 @@
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+apt-get update -y
+apt-get install docker-ce -y
 #systemctl status docker
 sudo usermod -aG docker ${USER}
 sudo su - ${USER}
