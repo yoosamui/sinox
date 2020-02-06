25b576f64d5befe3181c50d96e40b4193d40217b
Juan Gonzalez <j-gonzalez@email.de>
2020-02-07 03:53:18 +0700

    g

--- install-sinox.sh
+++ install-sinox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #-1
-sh ./update-fstab.sh
+
 
 #-2
 sh ./add-sudo.sh
@@ -16,7 +16,7 @@ sh ./sources_list.sh
 sh ./copy-files.sh
 
 #-6
-sh ./plymouth.sh
+
 
 #-7
 sh ./software-full.sh
@@ -36,4 +36,12 @@ sh ./optimizer.sh
 #-12
 sh ./post-install.sh
 
+
+
+
+
+sh ./plymouth.sh
+
+
+sh ./update-fstab.sh
 echo "done"
