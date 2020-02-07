0f386f40e78449f0a2ff4767972227dd84a0b068
Juan Gonzalez <j-gonzalez@email.de>
2020-02-07 09:58:05 +0700

    c

--- sources_list.sh
+++ sources_list.sh
@@ -2,7 +2,12 @@
 #--------------------
 # needs root access
 #--------------------
-cp -p etc/sources.list /etc/apt/sources.list
+#cp -p etc/sources.list /etc/apt/sources.list
+
+#buster backports
+echo "deb http://deb.debian.org/debian buster-backports main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian buster-backports main contrib non-free" >> /etc/apt/sources.list
+
 apt update
 apt upgrade -y
 
