c59dfcfc0296de3a3008b16eeff0f25190751575
Juan Gonzalez <j-gonzalez@email.de>
2020-02-07 16:07:57 +0700

    ff

--- install-sinox.sh
+++ install-sinox.sh
@@ -21,19 +21,28 @@ sh ./plymouth.sh
 #-7
 sh ./software-full.sh
 
+
+sh ./copy-files.sh
+
+
 #-8
 #sh ./update-alternatives.sh
 
+
+
+
+
+
 #-9
-sh ./docker.sh
+#sh ./docker.sh
 
 #-10
-sh ./software-extra.sh
+#sh ./software-extra.sh
 
 #-11
 #sh ./optimizer.sh
 
 #-12
-sh ./post-install.sh
+#sh ./post-install.sh
 
 echo "done"
