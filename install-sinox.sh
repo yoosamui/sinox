0f42b8d4d7ead0816ca3965b2bc716367a05b90b
Juan Gonzalez <j-gonzalez@email.de>
2020-02-18 13:16:19 +0700

    dock

--- install-sinox.sh
+++ install-sinox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sh ./add-sudo.sh
+sh ./sources_list.sh
 sh ./timezone-ntp.sh
 sh ./plymouth.sh
 sh ./software-full.sh
@@ -8,5 +9,4 @@ sh ./software-extra.sh
 sh ./docker.sh
 sh ./copy-files.sh
 sh ./update-fstab.sh
-#sh ./sources_list.sh
 sh ./post-install.sh
