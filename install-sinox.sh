462ebfc9b0a46d22f4623ead5b77fb1b51e67425
yoo <yoo@sinox.sinox.com>
2020-02-24 13:11:46 +0700

    a

--- install-sinox.sh
+++ install-sinox.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+sh ./timezone-ntp.sh
+sh ./add-sudo.sh
+sh ./sources_list.sh
+sh ./software-full.sh
+sh ./copy-files.sh
+#reboot
+sh ./plymouth.sh
+sh ./software-ui.sh
+sh ./software-extra.sh
+sh ./docker.sh
+sh ./post-install.sh
+sh ./update-fstab.sh
+exit
 
+#@//////////////////////
 sh ./add-sudo.sh
 sh ./sources_list.sh
 sh ./timezone-ntp.sh
