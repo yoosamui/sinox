72de96bd11e95cf0acd97b8c704c9dbcfbb07491
juan gonzalez <j-gonzalez@email.de>
2020-02-26 14:36:57 +0700

    Version 5

--- software-extra.sh
+++ software-extra.sh
@@ -27,6 +27,12 @@ nfs-common -y
 
 
 # samba 
+
+echo "[shared]" >>  /etc/samba/smb.conf
+echo "[path = /media/"$USER"/shared" >>  /etc/samba/smb.conf
+echo "valid users = $USER" >>  /etc/samba/smb.conf
+echo "read only = no" >>  /etc/samba/smb.conf
+cat /etc/samba/smb.conf
 echo "Please enter the password for samba. User="$USER
 sudo smbpasswd -a $USER
 
