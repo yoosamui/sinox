cb1b6996f3c5ff2dac3c9dea141dbf06c560a4a8
juan gonzalez <j-gonzalez@email.de>
2020-02-27 12:23:06 +0700

    N

--- install-sinox.sh
+++ install-sinox.sh
@@ -9,17 +9,17 @@ sh ./software-full.sh
 
 sh ./copy-files.sh
 
-#sh ./software-extra.sh
-#sh ./docker.sh
-#sh ./post-install.sh
-#sh ./optimizer.sh
-#sh ./update-fstab.sh
+sh ./software-extra.sh
+sh ./docker.sh
+sh ./post-install.sh
+sh ./optimizer.sh
+sh ./update-fstab.sh
 
 echo "--------------------------------------------------------------------------------------"
 echo "SINOX installation complete"
 echo "if you need install drivers do it now and then reboot."
 
-
+./
 cd drivers
 ls -al
 echo install driver
