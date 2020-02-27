64ff678eddcd58f25e5847dfff2a2ebe8a58b519
juan gonzalez <j-gonzalez@email.de>
2020-02-27 11:51:33 +0700

    N

--- software-extra.sh
+++ software-extra.sh
@@ -13,20 +13,18 @@ add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian buster 
 apt update
 echo "install skype" 
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
-skype_path='/home/'$USER'/sinox/skypeforlinux-64.deb'
+skype_path=$('skypeforlinux-64.deb')
 sudo dpkg -i ${skype_path}
 rm -f ${skype_path}
 
 
+# samba 
 
 apt install --fix-missing \
 samba \
 smbclient \
 cifs-utils \
 nfs-common -y
-
-
-# samba 
 
 echo "[shared]" >>  /etc/samba/smb.conf
 echo "path = /media/"$USER"/shared" >>  /etc/samba/smb.conf
@@ -35,6 +33,5 @@ echo "read only = no" >>  /etc/samba/smb.conf
 cat /etc/samba/smb.conf
 echo "Please enter the password for samba. User="$USER
 sudo smbpasswd -a $USER
-
 
 echo "done"
